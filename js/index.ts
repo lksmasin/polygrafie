ec9a1e08fc52da9b4a11f7d4dd1267248c1b99c9
lukymas <lksmasin@gmail.com>
2024-06-08 22:08:15 +0200

    Pokračování v pracích

--- js/index.ts
+++ js/index.ts
@@ -7,3 +7,7 @@ import '@material/web/icon/icon.js';
 import '@material/web/icon/internal/icon';
 import '@material/web/list/list.js';
 import '@material/web/list/list-item.js';
+import '@material/web/iconbutton/icon-button.js';
+import '@material/web/iconbutton/filled-icon-button.js';
+import '@material/web/iconbutton/filled-tonal-icon-button.js';
+import '@material/web/iconbutton/outlined-icon-button.js';
